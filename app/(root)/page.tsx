ffe273de970d3aa55b892ad6f1cc43819ad9ac8c
danielmarcos <daniel.marcos.1038@gmail.com>
2025-04-03 18:19:50 -0300

    feat: add StartupCard component and implement posts display, enhance date formatting utility

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: "Elon Musk" },
+    _id: 1,
+    description: "This is a description",
+    image: "https://cdn.theatlantic.com/thumbor/FPTWEk2jCD_GOlSx-Q3p04tDPOk=/302x50:4317x2308/1600x900/media/img/mt/2014/08/shutterstock_187027727-1/original.jpg",
+    category: "Robots",
+    title: "We Robots"
+  }]
 
   return (
     <>
@@ -15,6 +27,22 @@ export default async function Home({ searchParams }: {
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
