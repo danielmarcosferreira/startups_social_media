2e42632d0a58b7eedaabe6b127bbdb454c92c35b
danielmarcos <daniel.marcos.1038@gmail.com>
2025-04-05 20:37:46 -0300

    feat: integrate live content fetching with Sanity; update client configuration and add server-only dependency

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,13 @@ import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY })
 
   return (
     <>
@@ -36,6 +37,8 @@ export default async function Home({ searchParams }: {
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
