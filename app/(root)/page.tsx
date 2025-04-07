1f8d4f59505414a752dce7c8a23d74a0aa5903b6
danielmarcos <daniel.marcos.1038@gmail.com>
2025-04-06 21:02:01 -0300

    feat: implement authentication with GitHub; add user creation and session handling in NextAuth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,12 +3,18 @@ import SearchForm from "../../components/SearchForm";
 import { client } from "@/sanity/lib/client";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null }
+
+  const session = await auth();
+
+  console.log(session?.id)
+
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
 
   return (
